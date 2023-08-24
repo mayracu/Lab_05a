9b43419af0dcc455492e9cb8555343b31ff08273
MayraCuellar <mayrabeyota@hotmail.com>
2023-08-24 00:48:40 +0000

    prob3 solution

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ function sum(a, b) { //eslint-disable-line
   let firstValue = a + b;
   let secondValue = `The sum of ${a} and ${b} is ${firstValue}.`;
   return [firstValue, secondValue];
+  //return firstValue;
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
@@ -31,6 +32,7 @@ function multiply(a, b) { //eslint-disable-line
   let firstValue = a * b;
   let secondValue = `The product of ${a} and ${b} is ${firstValue}.`;
   return [firstValue, secondValue];
+  //return firstValue;
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -51,11 +53,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let firstSum = Number(sum(a, b)[0]);
+  let firstValue = Number(sum(firstSum, c)[0]);
 
+
+  let firstProduct = Number(multiply(a, b)[0]);
+  let secondValue = Number(multiply(firstProduct, c)[0]);
+
+  let thirdValue = `${a} and ${b} and ${c} sum to ${firstValue}.`;
+  let fourthValue = `The product of ${a} and ${b} and ${c} is ${secondValue}.`;
+
+  return [firstValue, secondValue,thirdValue, fourthValue];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
