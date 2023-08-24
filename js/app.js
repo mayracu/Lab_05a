9640356196ee8c233d50901e02d015021c059e57
MayraCuellar <mayrabeyota@hotmail.com>
2023-08-24 00:02:37 +0000

    prob2 solution

--- js/app.js
+++ js/app.js
@@ -28,11 +28,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let firstValue = a * b;
+  let secondValue = `The product of ${a} and ${b} is ${firstValue}.`;
+  return [firstValue, secondValue];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
